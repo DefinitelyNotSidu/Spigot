84dc62b5839101b58a2258bf056d311f2e36629e
DefinitelyNotSidu <b.magdz@gmail.com>
2020-08-22 05:08:08 +0200

    pierwszy import

---
+++ plugin/src/tutorialPlugin/SpigotBlankPlugin.java
@@ -0,0 +1,15 @@
+package tutorialPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotBlankPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
